e914fa3658ca0ed37a9229d47e11ad52c16156e1
jayleth72 <jay.letheby@proton.me>
2024-06-20 14:03:42 +1000

    added button to hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -13,12 +13,12 @@ const HeroSection = () => {
         <div className="absolute inset-0  z-10"></div>
   
         {/* Hero content */}
-        <div className="absolute inset-0 flex flex-col justify-center items-center text-center py-20 z-20">
+        <div className="flex items-end justify-center h-full px-4 pb-8">
           {/* <h1 className="text-4xl md:text-6xl font-bold">Welcome to Scones and IT</h1>
-          <p className="text-lg md:text-xl mt-4">Your Trusted IT Solution for the Mary Valley</p>
+          <p className="text-lg md:text-xl mt-4">Your Trusted IT Solution for the Mary Valley</p>*/}
           <button className="mt-8 px-6 py-3 bg-blue-500 text-white rounded-lg text-lg font-semibold hover:bg-blue-600">
-            Contact Us
-          </button> */}
+            Need Help?
+          </button> 
         </div>
       </div>
     );
