55e456fdf703b7840bfe2b38d46e1a9565883786
jayleth72 <jay.letheby@proton.me>
2024-07-04 13:55:30 +1000

    modified card sections

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -3,6 +3,7 @@ interface Service {
     description: string;
     icon: string; // Tailwind CSS utility class for the icon
     color: string; // Tailwind CSS utility class for the card background color
+    opacity: string;
   }
   
   const services: Service[] = [
@@ -11,44 +12,49 @@ interface Service {
       description: "Help and support for smartphone related issues .",
       icon: "fas fa-mobile", // Font Awesome icon for smartphone
       color: "bg-blue-500", // Tailwind CSS utility class for blue background
+      opacity: "bg-opacity-10"
     },
     {
       title: "Laptop & Desktop Solutions",
       description: "From upgrades to virus removal, we handle all your laptop and desktop needs.",
       icon: "fas fa-laptop", // Font Awesome icon for laptop
       color: "bg-green-500", // Tailwind CSS utility class for green background
+      opacity: "bg-opacity-10"
     },
     {
       title: "Printer Troubleshooting",
       description: "Just can't print anything?  We'll get you printing.",
       icon: "fas fa-print", // Font Awesome icon for printer
       color: "bg-yellow-500", // Tailwind CSS utility class for yellow background
+      opacity: "bg-opacity-10"
     },
     {
       title: "Internet Setup and problem solving",
       description: "Get connected with our efficient internet installation and troubleshooting services.",
       icon: "fas fa-wifi", // Font Awesome icon for Wi-Fi
       color: "bg-indigo-500", // Tailwind CSS utility class for indigo background
+      opacity: "bg-opacity-10"
     },
     {
       title: "AI Consultancy Services",
       description: "Harness the power of AI with our expert guidance and implementation solutions.",
       icon: "fas fa-robot", // Font Awesome icon for robot (AI)
       color: "bg-purple-500", // Tailwind CSS utility class for purple background
+      opacity: "bg-opacity-10"
     },
   ];
   
   function ServicesSection() {
     return (
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-4">
         {services.map((service) => (
           <div
             key={service.title}
-            className={`p-4 rounded-lg shadow-md ${service.color}`}
+            className={`p-4 rounded-lg shadow-lg ${service.color} ${service.opacity}`}
           >
             <i className={`text-4xl text-white ${service.icon}`} />
             <h2 className="text-2xl font-semibold mt-4">{service.title}</h2>
-            <p className="text-gray-700">{service.description}</p>
+            <p className="text-gray-800">{service.description}</p>
           </div>
         ))}
       </section>
